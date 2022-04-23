2f7d38d8b86a6a243aed4986aecd3479e24de4d8
nimoc <nimocc@126.com>
2022-04-23 18:56:20 +0800

    PrintIndent

--- x.go
+++ x.go
@@ -3,27 +3,37 @@ package xjson
 import "log"
 
 var emptyObjectAndArrayNotNull = true
-func emptyObjectString () string {
+
+func emptyObjectString() string {
 	if emptyObjectAndArrayNotNull {
 		return "{}"
 	} else {
 		return "null"
 	}
 }
-func emptyArrayString () string {
+func emptyArrayString() string {
 	if emptyObjectAndArrayNotNull {
 		return "[]"
 	} else {
 		return "null"
 	}
 }
+
 var autoNumberConvertString = true
 
 func Print(name string, v interface{}) {
-	data, err := MarshalIndent(v, "", "  ") ; if err != nil {
-	    log.Print(name, "\n", err)
+	data, err := Marshal(v)
+	if err != nil {
+		log.Print(name, "\n", err)
 	} else {
 		log.Print(name, "\n", string(data))
 	}
-
+}
+func PrintIndent(name string, v interface{}) {
+	data, err := MarshalIndent(v, "", "  ")
+	if err != nil {
+		log.Print(name, "\n", err)
+	} else {
+		log.Print(name, "\n", string(data))
+	}
 }
